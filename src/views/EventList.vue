1439d6c38eebd8fb29b794f97404c683a8383613
Patrik Näsström <patrik_nasstrom@hotmail.com>
2020-03-27 16:41:14 +0100

    updated  with vuex

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -8,27 +8,16 @@
 
 
 <script>
-import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
-
-export default {
-    components: {
+    import EventCard from '@/components/EventCard.vue'
+    import { mapState } from 'vuex'
+    
+    export default {
+      components: {
         EventCard
-    },
-    data() {
-        return{
-            events:[]
-        }
-    },
-    created() {
-        EventService.getEvents()
-        .then( response => {
-            this.events=response.data
-        })
-        .catch( error => {
-            console.log('There was an error:' + error.response)
-        })
+      },
+      created() {
+        this.$store.dispatch('fetchEvents')
+      },
+      computed: mapState(['events'])
     }
-
-}
-</script>
+    </script>
