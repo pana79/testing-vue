ce7b018f315fdc5467f56fcaddcbc08041d63451
Patrik Näsström <patrik_nasstrom@hotmail.com>
2020-04-01 08:37:34 +0200

    added modules User

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -68,7 +68,7 @@
               })
           },
         createFreshEventObject() {
-          const user = this.$store.state.user
+          const user = this.$store.state.User.user
           const id = Math.floor(Math.random()*10000000)
 
           return {
