7b8149fcd141da53ef4527b2f725f3cf32a1f197
Patrik Näsström <patrik_nasstrom@hotmail.com>
2020-04-01 09:29:19 +0200

    event and user moved to seperate  modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import EventService from "@/services/EventService.js"
 import * as User from "@/store/modules/User.js"
+import * as event from "@/store/modules/event.js"
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    User
+    User,
+    event
   },
   state: {
     categories: [
@@ -19,91 +20,5 @@ export default new Vuex.Store({
                   'food',
                   'community'
                 ],
-    todos: [
-      { id: 1, text: '...', done: true },
-      { id: 2, text: '...', done: false },
-      { id: 3, text: '...', done: true },
-      { id: 4, text: '...', done: false }
-    ],
-    eventsTotal: 0,
-    events: [],
-    event:{}
-  },
-  mutations: {
-    ADD_EVENT(state, event) {
-      state.events.push(event)
-    },
-    SET_EVENTS(state, events) {
-      state.events = events
-      //console.log('***SET_EVENT****')
-      //console.log(state.events)
-    },
-    SET_EVENTS_TOTAL(state, eventsTotal){
-      state.eventsTotal = eventsTotal
-    },
-    SET_EVENT(state, event){
-      state.event = event
-    }
-      
-  },
-  actions: {
-    createEvent({ commit }, event) {
-      return EventService.postEvent(event).then (() => {
-        commit('ADD_EVENT', event)
-      })
-    },
-    fetchEvents({ commit}, {perPage,page}) {
-      EventService.getEvents(perPage, page)
-        .then(response => {
-         // console.log(response.data)
-         console.log('Total events are ' + response.headers['x-total-count'])
-          commit('SET_EVENTS', response.data)
-          commit('SET_EVENTS_TOTAL',response.headers['x-total-count'])
-        
-        })
-        .catch(error => {
-          console.log('There was an error:', error.response)
-        })
-      },
-    fetchEvent({ commit, getters }, id) {
-      var event = getters.getEventById(id)
-
-      if(event) {
-        commit('SET_EVENT', event)
-      }
-      else {
-        EventService.getEvent(id)
-        .then(response => {
-            commit('SET_EVENT', response.data)
-        })
-        .catch(error => {
-            console.log('There was an error:', error.response)
-        })
-
-      }
-    
-    }
-  },
-  getters: {
-    getEventById: state => id => {
-      return state.events.find(event => event.id===id)
-
-    }
-
-
-    /*
-    catLength: state => {
-      return state.categories.length
-    },
-    doneTodos: state => {
-      return state.todos.filter(todo => todo.done)
-    },
-    activeTodosCount: state => {
-     return state.todos.filter(todo => !todo.done).lenght
-    }
-    you can even pass in a getter  to a getter like this
-    activeTodosCount: (state, getters) => {
-     return state.todos.lenght - getters.doneTodos.length
-     }*/
-  }
+  } 
 });
